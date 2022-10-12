445f92cd7a4bb6592d35b3b6724eb381bae3ae3f
Cyyyyyy1 <115411180+Cyyyyyy1@users.noreply.github.com>
2022-10-12 23:20:45 +0800

    Add files via upload

---
+++ Day 3/Day 3.md
@@ -0,0 +1,15 @@
+# Day 3
+
+**int**           *%d*
+
+**float**        *%f*
+
+**double**    *%lf*
+
+当局部变量和全局变量冲突时候，*优先***局部变量**
+
+scanf（“%d %d“，&a， &d）
+
+> scanf+**_CRT_SECURE_NO_WARNINGS**
+>
+> 
---
+++ Day 3/Day3/Day 3-1/Day 3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int age = 18;
+	float footmark = 41.5;
+	double weight = 53.5;
+	age = age + 1;
+	footmark = footmark - 0.3;
+	weight = weight - 10;
+	printf("%d\n", age);
+	printf("%f\n",footmark);
+	printf("%lf\n", weight);
+
+
+
+
+
+
+	return 0;
+}
